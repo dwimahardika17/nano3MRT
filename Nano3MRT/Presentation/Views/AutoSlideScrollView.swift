07fd90acf2cb315de9966ed535a133433aab973b
dwimahardika17 <130340667+dwimahardika17@users.noreply.github.com>
2023-07-21 18:01:28 +0700

    interface

---
+++ Nano3MRT/Presentation/Views/AutoSlideScrollView.swift
@@ -0,0 +1,20 @@
+//
+//  AutoSlideScrollView.swift
+//  Nano3MRT
+//
+//  Created by I MADE DWI MAHARDIKA on 21/07/23.
+//
+
+import SwiftUI
+
+struct AutoSlideScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AutoSlideScrollView_Previews: PreviewProvider {
+    static var previews: some View {
+        AutoSlideScrollView()
+    }
+}
---
+++ Nano3MRT/Presentation/Views/PlanCard.swift
@@ -0,0 +1,20 @@
+//
+//  PlanCard.swift
+//  Nano3MRT
+//
+//  Created by I MADE DWI MAHARDIKA on 21/07/23.
+//
+
+import SwiftUI
+
+struct PlanCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlanCard_Previews: PreviewProvider {
+    static var previews: some View {
+        PlanCard()
+    }
+}
